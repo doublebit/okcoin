4d1463e40243374e6e463bc052d2671ac49eff79
Vasile Goian <vasile.goian@doublebit.net>
2016-08-16 03:01:09 +0300

    Changed order of arguments and cleaned code a little

--- src/DoubleBit/OKCoin/Okcoin.php
+++ src/DoubleBit/OKCoin/Okcoin.php
@@ -17,16 +17,20 @@ class Okcoin
         $pieces = preg_split('/(?=[A-Z])/', $name);
         $method = $pieces[0];
         unset($pieces[0]);
-        $endpoint = strtolower(implode('_', $pieces)) . '.do';
-        $signature = $this->sign($arguments[0], $arguments[1]);
-        $query = http_build_query($arguments[0]);
+        $endpoint = strtolower(implode('_', $pieces));
+        
+        $api_key = $arguments[0];
+        $secret_key = $arguments[1];
+        $params = $origianl_params = isset($arguments[2]) ? $arguments[2] : [];
+        $callback = isset($arguments[3]) ? $arguments[3] : null;
+
+        $params['api_key'] = $api_key;
+        $signature = $this->sign($params, $secret_key);
+        $query = http_build_query($params);
         $query .= '&sign=' . $signature;
         $result = $this->callApi(strtoupper($method), $endpoint, $query);
-        if (isset($arguments[2]) && is_callable($arguments[2])) {
-            $strippedArgs = $arguments[0];
-            unset($strippedArgs['api_key']);
-            unset($strippedArgs['secret_key']);
-            call_user_func_array($arguments[2], [$endpoint, $strippedArgs, $result]);
+        if (is_callable($callback)) {
+            call_user_func_array($callback, [$endpoint, $original_params, $result]);
         }
         return $result;
     }
@@ -34,7 +38,7 @@ class Okcoin
     public function callApi($method, $endpoint, $query)
     {
         $client = new \GuzzleHttp\Client();
-        $url = 'https://www.okcoin.com/api/' . \Config::get('okcoin.api_version') . '/' . $endpoint . '?' . $query;
+        $url = 'https://www.okcoin.com/api/' . \Config::get('okcoin.api_version') . '/' . $endpoint . '.do?' . $query;
         $res = $client->request($method, $url);
         if ($res->getStatusCode() != 200) {
             return false;
